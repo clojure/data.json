dcd64845e9278c208938ee601536965057de3689
Tom Faulhaber <git_net@infolace.com>
2011-10-25 23:52:15 -0700

    Autodoc commit for master/1f00a45d

--- index-0.1.3.clj
+++ index-0.1.3.clj
@@ -1,7 +1,8 @@
 {:namespaces
  ({:source-url
-   "https://github.com/clojure/data.json/blob/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj",
-   :wiki-url "http://clojure.github.com/data.json/json-api.html",
+   "https://github.com/clojure/data.json/blob/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj",
+   :wiki-url
+   "http://clojure.github.com/data.json/clojure.data.json-api.html",
    :name "clojure.data.json",
    :author "Stuart Sierra",
    :doc
@@ -11,43 +12,43 @@
    :name "json-str",
    :namespace "clojure.data.json",
    :source-url
-   "https://github.com/clojure/data.json/blob/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj#L310",
+   "https://github.com/clojure/data.json/blob/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj#L306",
    :raw-source-url
-   "https://github.com/clojure/data.json/raw/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj",
+   "https://github.com/clojure/data.json/raw/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj",
    :wiki-url
-   "http://clojure.github.com/data.json//json-api.html#clojure.data.json/json-str",
+   "http://clojure.github.com/data.json//clojure.data.json-api.html#clojure.data.json/json-str",
    :doc
    "Converts x to a JSON-formatted string.\n\nValid options are:\n  :escape-unicode false\n      to turn of \\uXXXX escapes of Unicode characters.",
    :var-type "function",
-   :line 310,
+   :line 306,
    :file "src/main/clojure/clojure/data/json.clj"}
   {:arglists ([x & options]),
    :name "pprint-json",
    :namespace "clojure.data.json",
    :source-url
-   "https://github.com/clojure/data.json/blob/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj#L357",
+   "https://github.com/clojure/data.json/blob/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj#L353",
    :raw-source-url
-   "https://github.com/clojure/data.json/raw/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj",
+   "https://github.com/clojure/data.json/raw/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj",
    :wiki-url
-   "http://clojure.github.com/data.json//json-api.html#clojure.data.json/pprint-json",
+   "http://clojure.github.com/data.json//clojure.data.json-api.html#clojure.data.json/pprint-json",
    :doc
    "Pretty-prints JSON representation of x to *out*.\n\nValid options are:\n  :escape-unicode false\n      to turn off \\uXXXX escapes of Unicode characters.",
    :var-type "function",
-   :line 357,
+   :line 353,
    :file "src/main/clojure/clojure/data/json.clj"}
   {:arglists ([x & options]),
    :name "print-json",
    :namespace "clojure.data.json",
    :source-url
-   "https://github.com/clojure/data.json/blob/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj#L323",
+   "https://github.com/clojure/data.json/blob/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj#L319",
    :raw-source-url
-   "https://github.com/clojure/data.json/raw/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj",
+   "https://github.com/clojure/data.json/raw/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj",
    :wiki-url
-   "http://clojure.github.com/data.json//json-api.html#clojure.data.json/print-json",
+   "http://clojure.github.com/data.json//clojure.data.json-api.html#clojure.data.json/print-json",
    :doc
    "Write JSON-formatted output to *out*.\n\nValid options are:\n  :escape-unicode false\n      to turn off \\uXXXX escapes of Unicode characters.",
    :var-type "function",
-   :line 323,
+   :line 319,
    :file "src/main/clojure/clojure/data/json.clj"}
   {:arglists
    ([input]
@@ -56,21 +57,21 @@
    :name "read-json",
    :namespace "clojure.data.json",
    :source-url
-   "https://github.com/clojure/data.json/blob/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj#L186",
+   "https://github.com/clojure/data.json/blob/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj#L182",
    :raw-source-url
-   "https://github.com/clojure/data.json/raw/ef6a05f6599862a8c5c7b0d00fe4ce418fc638be/src/main/clojure/clojure/data/json.clj",
+   "https://github.com/clojure/data.json/raw/9515a71782aeb7f8c75d189584a652fb72decd18/src/main/clojure/clojure/data/json.clj",
    :wiki-url
-   "http://clojure.github.com/data.json//json-api.html#clojure.data.json/read-json",
+   "http://clojure.github.com/data.json//clojure.data.json-api.html#clojure.data.json/read-json",
    :doc
    "Reads one JSON value from input String or Reader.\nIf keywordize? is true (default), object keys will be converted to\nkeywords.  If eof-error? is true (default), empty input will throw\nan EOFException; if false EOF will return eof-value. ",
    :var-type "function",
-   :line 186,
+   :line 182,
    :file "src/main/clojure/clojure/data/json.clj"}
   {:file nil,
    :raw-source-url nil,
    :source-url nil,
    :wiki-url
-   "http://clojure.github.com/data.json//json-api.html#clojure.data.json/read-json-from",
+   "http://clojure.github.com/data.json//clojure.data.json-api.html#clojure.data.json/read-json-from",
    :namespace "clojure.data.json",
    :var-type "function",
    :arglists ([input keywordize? eof-error? eof-value]),
@@ -81,7 +82,7 @@
    :raw-source-url nil,
    :source-url nil,
    :wiki-url
-   "http://clojure.github.com/data.json//json-api.html#clojure.data.json/write-json",
+   "http://clojure.github.com/data.json//clojure.data.json-api.html#clojure.data.json/write-json",
    :namespace "clojure.data.json",
    :var-type "function",
    :arglists ([object out escape-unicode?]),
