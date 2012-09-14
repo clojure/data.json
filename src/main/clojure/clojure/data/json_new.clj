30149841e1d883eb2eb7fa341683e8a69d6247bb
Stuart Sierra <mail@stuartsierra.com>
2012-09-14 14:06:18 -0400

    Update copyright notice in source

--- src/main/clojure/clojure/data/json_new.clj
+++ src/main/clojure/clojure/data/json_new.clj
@@ -1,4 +1,4 @@
-;; Copyright (c) Stuart Sierra, 2010. All rights reserved.  The use
+;; Copyright (c) Stuart Sierra, 2012. All rights reserved.  The use
 ;; and distribution terms for this software are covered by the Eclipse
 ;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
 ;; which can be found in the file epl-v10.html at the root of this
