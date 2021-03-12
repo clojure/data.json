89d0eeb2b4e35dbd8601862d682e30fe6eab4b05
Alex Miller <alex.miller@cognitect.com>
2021-03-12 10:07:35 -0600

    split lein perf test more fully from main source

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
+;; NOTE: Used only for perf testing - this project is built with Maven (see pom.xml)
 (defproject clojure.data.json "1.1.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
   :java-test-paths ["src/test/java"]
-  :test-paths ["src/test/clojure"]
+  :test-paths ["src/test/clojure" "src/test/clojure-perf"]
   :profiles {:dev {:dependencies [[com.clojure-goes-fast/clj-async-profiler "0.5.0"]
                                   [com.clojure-goes-fast/clj-java-decompiler "0.3.0"]
                                   [criterium/criterium "0.4.6"]
