12344bbb98ae3070607268f210f393a64f5051dd
Alex Miller <alex.miller@cognitect.com>
2021-07-12 15:25:11 -0500

    move 0.1.x deprecated api from separate file into main ns

--- src/main/clojure/clojure/data/json.clj
+++ src/main/clojure/clojure/data/json.clj
@@ -741,9 +741,69 @@
     (pprint/with-pprint-dispatch #(pprint-dispatch % opts)
       (pprint/pprint x))))
 
-(load "json_compat_0_1")
+;; DEPRECATED APIs from 0.1.x
 
-;; Local Variables:
-;; mode: clojure
-;; eval: (define-clojure-indent (codepoint-case (quote defun)))
-;; End:
+(defn read-json
+  "DEPRECATED; replaced by read-str.
+
+  Reads one JSON value from input String or Reader. If keywordize? is
+  true (default), object keys will be converted to keywords. If
+  eof-error? is true (default), empty input will throw an
+  EOFException; if false EOF will return eof-value."
+  ([input]
+   (read-json input true true nil))
+  ([input keywordize?]
+   (read-json input keywordize? true nil))
+  ([input keywordize? eof-error? eof-value]
+   (let [key-fn (if keywordize? keyword identity)]
+     (condp instance? input
+       String
+       (read-str input
+         :key-fn key-fn
+         :eof-error? eof-error?
+         :eof-value eof-value)
+       java.io.Reader
+       (read input
+         :key-fn key-fn
+         :eof-error? eof-error?
+         :eof-value eof-value)))))
+
+(defn write-json
+  "DEPRECATED; replaced by 'write'.
+
+  Print object to PrintWriter out as JSON"
+  [x out escape-unicode?]
+  (write x out :escape-unicode escape-unicode?))
+
+(defn json-str
+  "DEPRECATED; replaced by 'write-str'.
+
+  Converts x to a JSON-formatted string.
+
+  Valid options are:
+    :escape-unicode false
+        to turn of \\uXXXX escapes of Unicode characters."
+  [x & options]
+  (apply write-str x options))
+
+(defn print-json
+  "DEPRECATED; replaced by 'write' to *out*.
+
+  Write JSON-formatted output to *out*.
+
+  Valid options are:
+    :escape-unicode false
+        to turn off \\uXXXX escapes of Unicode characters."
+  [x & options]
+  (apply write x *out* options))
+
+(defn pprint-json
+  "DEPRECATED; replaced by 'pprint'.
+
+  Pretty-prints JSON representation of x to *out*.
+
+  Valid options are:
+    :escape-unicode false
+        to turn off \\uXXXX escapes of Unicode characters."
+  [x & options]
+  (apply pprint x options))
